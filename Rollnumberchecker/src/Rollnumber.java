a503a4615575e1d01fb60368ab48924b4b25b65d
DINBANDHU <DINBANDHU@DESKTOP-AR09I34>
2022-08-18 17:04:07 +0530

    Rollnuberchecker

--- Rollnumberchecker/src/Rollnumber.java
+++ Rollnumberchecker/src/Rollnumber.java
@@ -3,37 +3,37 @@ public class Rollnumber {
 
 	public static void main(String[] args) {
 			 Scanner sc = new Scanner(System.in);
-			
-				int[] a = {1,5,9,13,17,21};
-				int[] b = {2,6,10,14,18,22};
-				int[] c = {3,7,11,15,19,23};
-				int[] d = {4,8,12,16,20,24};
 				System.out.println("Input roll number ");
 				//Roll number as input
 				int roll = sc.nextInt();
-				for(int i:a)
+				int a=1,b=2,c=3,d=4;
+				int x=0;
+				do
+				{
+					if(roll==a)
 					{
-					if(i==roll)
-					{System.out.println("your group is Sapphire");}
+						System.out.println("Sapphire ");
+						x=1;
 					}
-				for(int j:b)
+					if(roll==b)
 					{
-					if(j==roll)
-					{System.out.println("your group is Pearl");}
+						System.out.println("Pearl ");
+						x=1;
 					}
-				for(int k:c)
+					if(roll==c)
 					{
-					if(k==roll)
-					{System.out.println("your group is Ruby");}
+						System.out.println("Ruby ");
+						x=1;
 					}
-				for(int l:d)
+					if(roll==d)
 					{
-					if(l==roll)
-					{System.out.println("your group is Emerald");}
+						System.out.println("Emerald");
+						x=1;
 					}
+					a+=4;
+					b+=4;
+					c+=4;
+					d+=4;
+				}while(x==0);
 			}
-		
-
-	}
-
-
+		}
